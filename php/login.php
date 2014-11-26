8f7d285561337e9d3babff6496ebd6489a8542b8
Luca Coviello <luca.coviello@gmail.com>
2014-11-26 18:06:04 +0100

    coviello

--- php/login.php
+++ php/login.php
@@ -5,4 +5,6 @@ setcookie( "name", $_POST['username'], 0, '/') or die('/ntryng');
 header("Location: http://".$_SERVER['HTTP_HOST']."/aule.php");
 die("aiuto aiuto");
 
+echo "coviello cagna canshielloooo"
+
 ?>
