a1a5c491bfb21f453b99440e43de4550cf82e638
Luca Coviello <luca.coviello@gmail.com>
2014-11-26 16:18:03 +0100

    First Commit

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
-<?php
-echo "ciao brutti stronzi denis culo"
-?>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<title>AulApp</title>
+		<link rel="stylesheet" href="style.css" type="text/css">
+	</head>
+	<body>
+		<center>
+			<header><h1>A<span>ul</span>A<span>pp</span></h1></header>
+
+			<div id="divimg">
+				<img src="images/logo.png"/>
+			</div>
+
+			<div id="divtesto">
+				<p>Scopri un mondo di stanze<br/>fatte su misura per te!<br/><br/><span>#divertimentoportamivia!!</span></p>
+			</div>
+
+			<div id="divdwnl">
+				<div class="separatore"></div>
+				<a href="index.php">Login</a>
+			</div>
+
+			<footer>&copy; 2014 <b>Brunella</b> All rights reserved.</footer>
+		</center>
+	</body>
+</html>
